7d2ef63214884746ee9efe33ee62859403fa30b1
Sasha <sasha@Sashas-MacBook-Air.local>
2020-05-10 11:19:37 +0545

    Work on examples page

--- containers/contact/index.js
+++ containers/contact/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import { Col } from 'components';
+import { Col, S1 } from 'components';
 
 function Contact({}) {
-  return <Col alignItems="center">Contact me</Col>;
+  return (
+    <Col alignItems="center">
+      <div />
+      <S1>Contact me for some language magic</S1>
+    </Col>
+  );
 }
 
 export default Contact;
--- containers/portfolio.js
+++ containers/portfolio.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Row } from 'components';
+import { Row, Col } from 'components';
 
-const PageStyle = styled(Row)`
+const PageStyle = styled(Col)`
   background: #ffcc88;
   height: 100%;
   align-items: center;
+  justify-content: space-between;
 `;
 
-const Boxes = styled(Row)`
-  background: whitesmoke;
-  width: 50%;
-  height: 80%;
-  flex-wrap: wrap;
-  align-itmes: ;
+const Header = styled(Row)`
+  min-height: 30px;
 `;
+
+const Boxes = styled(Col)`
+  background: whitesmoke;
+  width: 30%;
+`;
+
 const Preview = styled(Row)`
   background: whitesmoke;
-  width: 50%;
-  height: 80%;
+  width: 55%;
+  height: 70%;
+  margin-right: 3%;
   flex-wrap: wrap;
   align-itmes: ;
   border: 1px solid black;
@@ -27,11 +31,11 @@ const Preview = styled(Row)`
 
 const Box = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: start;
+  justify-content: start;
 
   background: red;
-  width: 30%;
+
   border: 1px solid black;
   margin: 20px;
   padding: 10px;
@@ -41,15 +45,18 @@ const Box = styled.div`
 function WhyPickMe() {
   return (
     <PageStyle>
-      <Boxes>
-        <Box>First Box</Box>
-        <Box>Second Box</Box>
-        <Box>Third Box</Box>
-        <Box>Forth Box</Box>
-        <Box>Fifth Box</Box>
-        <Box>Sixth Box</Box>
-      </Boxes>
-      <Preview>Right Panel</Preview>
+      <Header>How I Translate</Header>
+      <Row>
+        <Boxes>
+          <Box>First Box</Box>
+          <Box>Second Box</Box>
+          <Box>Third Box</Box>
+          <Box>Forth Box</Box>
+          <Box>Fifth Box</Box>
+          <Box>Sixth Box</Box>
+        </Boxes>
+        <Preview>Right Panel</Preview>
+      </Row>
     </PageStyle>
   );
 }
