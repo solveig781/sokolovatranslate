fb88416055e37a128e66e9d458dc21f3be5adad0
Sasha <sasha@Sashas-MacBook-Air.local>
2020-05-10 11:44:13 +0545

    Added missing icons for contact page

--- containers/contact/index.js
+++ containers/contact/index.js
@@ -3,10 +3,17 @@ import styled from 'styled-components';
 
 import { Col, S1 } from 'components';
 
+import LinkedInIcon from './linkedin';
+import EmailIcon from './email';
+import WandIcon from './wand';
+
 function Contact({}) {
   return (
     <Col alignItems="center">
       <div />
+      <LinkedInIcon />
+      <EmailIcon />
+      <WandIcon />
       <S1>Contact me for some language magic</S1>
     </Col>
   );
